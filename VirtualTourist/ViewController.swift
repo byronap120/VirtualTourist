c0eb84d1b4efe82f8bd2e93d6f5207af9398c565
byronajin <byronap120@gmail.com>
2020-06-19 11:04:10 -0600

    Initial Commit

---
+++ VirtualTourist/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  VirtualTourist
+//
+//  Created by Byron Ajin on 6/19/20.
+//  Copyright © 2020 Byron Ajin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
