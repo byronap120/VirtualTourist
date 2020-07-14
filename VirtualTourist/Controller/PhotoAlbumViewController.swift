5623a1ca45bbe8cbfe03bd9b3d9c5365bc1a22ca
byronajin <byronap120@gmail.com>
2020-07-14 16:43:55 -0600

    fix collection cell dimension

--- VirtualTourist/Controller/PhotoAlbumViewController.swift
+++ VirtualTourist/Controller/PhotoAlbumViewController.swift
@@ -14,11 +14,16 @@ class PhotoAlbumViewController: UIViewController, UICollectionViewDataSource, UI
     
     @IBOutlet weak var mapView: MKMapView!
     @IBOutlet weak var collectionView: UICollectionView!
+    @IBOutlet weak var flowLayout: UICollectionViewFlowLayout!
     
     private let titleErrorMessage = "Error"
     private let coreDataErrorMessage = "Core Data error: "
     private let alertMessageTitle = "Alert"
     private let alertMessage = "No Images for this location"
+    private let numberOfCellsPortrait: CGFloat = 3.0
+    private let numberOfCellsLandscape: CGFloat = 6.0
+    private let minimunSpace: CGFloat = 1.0
+    private let dimensionSpace: CGFloat = 6.0
     private var arePhotosFromNetwork = false
     private var photosUrlList: [PhotoUrl] = []
     private var photos: [Photo] = []
@@ -28,8 +33,23 @@ class PhotoAlbumViewController: UIViewController, UICollectionViewDataSource, UI
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         self.navigationController?.setNavigationBarHidden(false, animated: true)
+        updateFlowLayoutDimensions(size: view.frame.size, numberOfCells: numberOfCellsPortrait)
         updateMapViewLocation()
         getLocalPhotos()
+    }
+    
+    private func updateMapViewLocation(){
+        let location = CLLocationCoordinate2D(latitude: pin.latitude, longitude: pin.longitude)
+        var region: MKCoordinateRegion = self.mapView.region
+        region.center = location
+        region.span.longitudeDelta /= 8.0
+        region.span.latitudeDelta /= 8.0
+        self.mapView.setRegion(region, animated: true)
+        
+        let annotation = MKPointAnnotation()
+        annotation.coordinate = location
+        self.mapView.addAnnotation(annotation)
+        self.mapView.selectAnnotation(annotation, animated: true)
     }
     
     private func getLocalPhotos(){
@@ -72,30 +92,6 @@ class PhotoAlbumViewController: UIViewController, UICollectionViewDataSource, UI
         collectionView.reloadData()
     }
     
-    private func updateMapViewLocation(){
-        let location = CLLocationCoordinate2D(latitude: pin.latitude, longitude: pin.longitude)
-        var region: MKCoordinateRegion = self.mapView.region
-        region.center = location
-        region.span.longitudeDelta /= 8.0
-        region.span.latitudeDelta /= 8.0
-        self.mapView.setRegion(region, animated: true)
-        
-        let annotation = MKPointAnnotation()
-        annotation.coordinate = location
-        self.mapView.addAnnotation(annotation)
-        self.mapView.selectAnnotation(annotation, animated: true)
-    }
-    
-    private func showIndicatorOnCell(cell: PhotoCollectionViewCell , show: Bool){
-        if(show) {
-            cell.progressIndicator.startAnimating()
-            cell.progressIndicator.isHidden = false
-        } else {
-            cell.progressIndicator.stopAnimating()
-            cell.progressIndicator.isHidden = true
-        }
-    }
-    
     private func saveNewPhoto(url: String, data: Data){
         guard let managedContext = getManagedContext() else{
             return
@@ -113,14 +109,29 @@ class PhotoAlbumViewController: UIViewController, UICollectionViewDataSource, UI
         }
     }
     
-    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+    private func showIndicatorOnCell(cell: PhotoCollectionViewCell , show: Bool){
+        if(show) {
+            cell.progressIndicator.startAnimating()
+            cell.progressIndicator.isHidden = false
+        } else {
+            cell.progressIndicator.stopAnimating()
+            cell.progressIndicator.isHidden = true
+        }
+    }
+    
+    private func updateFlowLayoutDimensions(size: CGSize, numberOfCells: CGFloat){
+        let dimension = (size.width - dimensionSpace) / numberOfCells
+        flowLayout?.minimumInteritemSpacing = minimunSpace
+        flowLayout?.minimumLineSpacing = minimunSpace
+        flowLayout?.itemSize = CGSize(width: dimension, height: dimension)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int{
         return arePhotosFromNetwork ? photosUrlList.count : photos.count
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "PhotoViewCell", for: indexPath) as! PhotoCollectionViewCell
-        
         showIndicatorOnCell(cell: cell, show: true)
         if(arePhotosFromNetwork) {            
             let photoUrl = photosUrlList[indexPath.item].url
